312ac32942118f7f8b70f47255d9afcaeae2d22d
Sebastien Le Maguer <lemagues@tcd.ie>
2019-01-18 17:59:57 +0000

    Synthesize.java: a little bit of love formatting

--- buildSrc/src/main/java/marytts/Synthesize.java
+++ buildSrc/src/main/java/marytts/Synthesize.java
@@ -6,7 +6,11 @@ import java.io.File;
 import java.io.PrintWriter;
 import java.nio.file.*;
 
-// Configuration
+// Logger
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
+
+// Mary configuration
 import marytts.config.MaryConfiguration;
 import marytts.config.JSONMaryConfigLoader;
 
@@ -21,6 +25,8 @@ import marytts.runutils.Request;
  */
 public class Synthesize
 {
+    protected static Logger logger = LogManager.getLogger(Synthesize.class.toString());
+
     public static void main(String[] args) throws Exception {
         File input_dir = new File(args[0]);
         File output_dir = new File(args[1]);
@@ -39,14 +45,18 @@ public class Synthesize
             String input_text =  new String(Files.readAllBytes(text_file.toPath()));
 
             // Achieve the request
+
+            logger.info("=================================================================================");
             Request request = new Request(configuration, input_text);
             request.process();
+            String output = request.serializeFinaleUtterance().toString();
+            logger.info("=================================================================================\n");
 
             // Dump the output
             output_dir.mkdirs();
             File output_file = new File(output_dir, text_file.getName() + ".xml");
             PrintWriter writer = new PrintWriter(output_file);
-            writer.println(request.serializeFinaleUtterance().toString());
+            writer.println(output);
             writer.close();
         }
 
